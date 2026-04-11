b9c108303f0757fe40ee1b3e7fb2025ef1be6e40
takxs <paezrinconfelipe7@gmail.com>
2026-04-11 10:57:41 -0500

    Datos iniciales y superusuario automatico

--- FPC_Project/build.sh
+++ FPC_Project/build.sh
@@ -1,1 +1,17 @@
-pip install -r requirements.txt && python manage.py collectstatic --no-input && python manage.py migrate
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Crea el superusuario usando las variables de entorno de Render
+if [ "$CREATE_SUPERUSER" ]; then
+  python manage.py createsuperuser --no-input || true
+fi
+
+# Carga los datos si el archivo existe
+if [ -f iniciales.json ]; then
+  python manage.py loaddata iniciales.json
+fi
